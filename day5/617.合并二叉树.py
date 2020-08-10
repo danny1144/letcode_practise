417cde5662dc59e2a1af657f874541f94039c14c
xiaoping.zhong.ext <xiaoping.zhong.ext@siemens.com>
2020-08-10 14:02:58 +0800

    合并二叉树

---
+++ day5/617.合并二叉树.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=617 lang=python3
+#
+# [617] 合并二叉树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if t1 is None:
+            return t2
+        if t2 is None:
+            return t1
+        root = TreeNode(t1.val + t2.val)
+        root.left = self.mergeTrees(t1.left, t2.left)
+        root.right = self.mergeTrees(t1.right, t2.right)
+        return root
+
+# @lc code=end
